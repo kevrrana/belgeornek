d5115afbed307bca2b8855b2f7a1e6444d713908
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-09 14:58:15 +0300

    Add files via upload

---
+++ belgeornek1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace belgeornek1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKonturolEt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKonturolEt
+            // 
+            this.btnKonturolEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKonturolEt.Location = new System.Drawing.Point(146, 131);
+            this.btnKonturolEt.Name = "btnKonturolEt";
+            this.btnKonturolEt.Size = new System.Drawing.Size(165, 29);
+            this.btnKonturolEt.TabIndex = 0;
+            this.btnKonturolEt.Text = "Konturol Et";
+            this.btnKonturolEt.UseVisualStyleBackColor = true;
+            this.btnKonturolEt.Click += new System.EventHandler(this.btnKonturolEt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOrtalama.Location = new System.Drawing.Point(146, 96);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(165, 29);
+            this.txtOrtalama.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ortalamanız:";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(70, 202);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(70, 20);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "Sonuç :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKonturolEt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKonturolEt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ belgeornek1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace belgeornek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "";
+        }
+
+        private void btnKonturolEt_Click(object sender, EventArgs e)
+        {
+            float ortalama;
+            ortalama = Convert.ToSingle(txtOrtalama.Text);
+
+            if (ortalama < 0)
+            {
+                lblSonuc.Text = "Negatif Değer Giremezsin.";
+            }
+            else if (ortalama < 50)
+            {
+                lblSonuc.Text = "Kaldızın.";
+            }
+            else if (ortalama < 70)
+            {
+                lblSonuc.Text = "Tebrikler. Geçtiniz.";
+            }
+            else if (ortalama < 85)
+            {
+                lblSonuc.Text = "Teşekkür Belgesi aldınızz";
+            }
+            else if (ortalama <= 100)
+            {
+                lblSonuc.Text = "Taktir aldınızz";
+            }
+           
+        }
+    }
+}
